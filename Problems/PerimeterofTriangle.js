64d4c72e0fae4853b267249e500b4129de81db1b
prince-0303 <princebinil2003@gmail.com>
2025-06-12 18:53:36 +0530

    Peremeter of Rectangle

---
+++ Problems/PerimeterofTriangle.js
@@ -0,0 +1,9 @@
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+
+function peremeter (length, width) {
+    return (length+width)*2
+}
+
+console.log(peremeter(6,7))
+console.log(peremeter(20,10))
+console.log(peremeter(2,9))
