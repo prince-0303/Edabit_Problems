8305b4f67b0053c3e7a1b92a6745303b961d1103
prince-0303 <princebinil2003@gmail.com>
2025-06-12 18:42:01 +0530

    Convert Age to Days

---
+++ Problems/AgetoDays.js
@@ -0,0 +1,11 @@
+// Convert Age to Dayscd
+
+
+function calcAge(age){
+    return age * 365
+}
+console.log(calcAge(20))
+console.log(calcAge(15))
+console.log(calcAge(50))
+console.log(calcAge(27))
+console.log(calcAge(89))
