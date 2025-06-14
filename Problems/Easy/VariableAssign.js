32173f25ae37dabc74afc9ae68221b9faf29e101
prince-0303 <princebinil2003@gmail.com>
2025-06-14 17:50:34 +0530

    Basic Variable Assignment

---
+++ Problems/Easy/VariableAssign.js
@@ -0,0 +1,14 @@
+// Basic Variable Assignment
+
+// A student learning JavaScript was trying to make a function.
+// His code should concatenate a passed string name with string "Edabit" and store it in a variable called result.
+// He needs your help to fix this code.
+
+function nameString(name){
+	var b = "Edabit"
+	var result = name + b
+  	return result
+}
+console.log(nameString('Prince'));
+console.log(nameString('Matt'));
+console.log(nameString('javaScript'));
