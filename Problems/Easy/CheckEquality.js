de5361fc2e1740aae9a93e1c7711e5c66f53f7aa
prince-0303 <princebinil2003@gmail.com>
2025-06-15 15:35:58 +0530

    Equality Check

---
+++ Problems/Easy/CheckEquality.js
@@ -0,0 +1,9 @@
+// Equality Check
+
+function checkEquality(a, b) {
+ return ((a===b) ? true : false); 
+}
+
+console.log(checkEquality(1, true))
+console.log(checkEquality(0, "0"))
+console.log(checkEquality(1, 1))
