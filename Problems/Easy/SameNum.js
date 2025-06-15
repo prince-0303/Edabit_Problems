852663713ddf9f4bbdc4e6fde0247e398c9bfd08
prince-0303 <princebinil2003@gmail.com>
2025-06-15 15:10:02 +0530

    Same number ?

---
+++ Problems/Easy/SameNum.js
@@ -0,0 +1,9 @@
+// Are the Numbers Equal?
+
+function isSameNum(num1, num2) {
+	return (num1 === num2)
+}
+console.log(isSameNum(2,2))
+console.log(isSameNum(2,3))
+console.log(isSameNum(3,3))
+console.log(isSameNum(4,3))
